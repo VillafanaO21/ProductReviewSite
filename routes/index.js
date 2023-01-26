45cc66159d68348fff7f206b6008ba2ba2b6ea8a
rhsb <ovillafana65@gmail.com>
2023-01-26 11:31:02 -0800

    1 commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const productController = require('../controller/productController.js')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/products/profile/:id', productController.renderProfile );
+module.exports = router;
