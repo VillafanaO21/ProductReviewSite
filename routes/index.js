1f8150fd9e0d09cd1ec6f441fd1b77c0a4f5ec14
rhsb <ovillafana65@gmail.com>
2023-02-06 11:37:40 -0800

    5 commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controller/productController.js')
+const reviewController = require('../controller/reviewController.js')
 
 router.get('/', function(req, res, next){
   res.render('index', {title: 'express'});
@@ -19,4 +20,9 @@ router.post('/products/add', productController.addProduct);
 
 router.get('/products/delete/:id', productController.deleteProduct);
 
+router.get('/products/:productId/reviews/add', reviewController.renderAddReviewForm);
+
+router.post('/products/:productId/reviews/add', reviewController.addReview);
+
+router.get('/reviews/edit/:id', reviewController.renderEditReviewForm);
 module.exports = router;
